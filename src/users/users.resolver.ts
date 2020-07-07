65c194c1793103855319dd5a709d202954f6c43c
HaNguyenQuang <nguyenquangha0209@gmail.com>
2020-07-07 17:39:15 +0700

    create function graphql

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,4 +1,5 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { UpdateResult, DeleteResult } from 'typeorm';
 import { UsersDTO } from './users.dto';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
@@ -7,12 +8,25 @@ import { User } from './user.entity';
 export class UsersResolver {
   constructor(private usersService: UsersService) {}
 
-  // @Query(returns => [UsersDTO])
-  // async getAll(): Promise<UsersDTO[]> {
-  //   return await this.usersService.getUsers();
-  // }
   @Query('users')
   async getUser(){
     return await this.usersService.getUsers();
+  }
+
+  @Mutation('create')
+  async create(@Args('inputUser') inputUser: User): Promise<User> {
+    return this.usersService.createUser(inputUser);
+  }
+
+  @Mutation('update')
+  async update(@Args('user')
+    user: User,
+  ): Promise<UpdateResult> {
+    return this.usersService.updateUser(user);
+  }
+
+  @Mutation('delete')
+  async delete(@Args('id') id: number): Promise<DeleteResult> {
+    return this.usersService.deleteUser(id);
   }
 }
